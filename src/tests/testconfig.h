23939af05c67c40c0c7e311ecf38992347b2db0d
Satoru NAKAJIMA <c5h12@mail.goo.ne.jp>
2022-06-18 20:25:21 +0900

    delete generated file

--- src/tests/testconfig.h
+++
@@ -1,7 +0,0 @@
-#ifndef PINKYPI_TEST_CONFIG_H
-#define PINKYPI_TEST_CONFIG_H
-
-#define PINKYPI_TEST_DATA_DIR   "/Users/satoru/Documents/_Working/GitRepos/RaytracingCamp2019-22/src/tests/testdata"
-#define PINKYPI_TEST_OUTPUT_DIR "/Users/satoru/Documents/_Working/GitRepos/RaytracingCamp2019-22/src/tests/output"
-
-#endif
