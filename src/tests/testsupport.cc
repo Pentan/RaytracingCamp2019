1596d7f4ad9f389a461cbf841bcc62a120898aaf
Satoru NAKAJIMA <c5h12pentan@gmail.com>
2022-06-24 04:10:36 +0900

    use filesystem

--- src/tests/testsupport.cc
+++ src/tests/testsupport.cc
@@ -1,29 +1,49 @@
 #include <sstream>
-
 #include <cstdio>
-#include <dirent.h>
-#include <sys/stat.h>
-
 #include "testsupport.h"
 
 using namespace PinkyPi;
 
-void PinkyPi::CheckTestOutputDir(std::string dirpath) {
-    DIR *dir = opendir(PINKYPI_TEST_OUTPUT_DIR);
+#ifdef PINKYPI_HAS_STD_FILESYSTEM
+#include <filesystem>
+namespace {
+bool CheckExistsOrCreateDir(std::string dirpath) {
+    const std::filesystem::path path(dirpath);
+    if(!std::filesystem::exists(path)) {
+        if(!std::filesystem::create_directory(path)) {
+            return false;
+        }
+    }
+    return true;
+}
+}
+#else
+#include <dirent.h>
+#include <sys/stat.h>
+namespace {
+bool CheckExistsOrCreateDir(std::string dirpath) {
+    DIR *dir = opendir(dirpath.c_str());
     if(dir == NULL) {
-        mkdir(PINKYPI_TEST_OUTPUT_DIR, ACCESSPERMS);
+        if(mkdir(dirpath.c_str(), ACCESSPERMS) != 0) {
+            return false;
+        }
     } else {
         closedir(dir);
+    }
+    return true;
+}
+}
+#endif
+
+void PinkyPi::CheckTestOutputDir(std::string dirpath) {
+    if(!CheckExistsOrCreateDir(PINKYPI_TEST_OUTPUT_DIR)) {
+        return;
     }
     
     std::stringstream ss;
     ss << PINKYPI_TEST_OUTPUT_DIR << "/" << dirpath;
     std::string fullpath = ss.str();
-    
-    dir = opendir(fullpath.c_str());
-    if(dir == NULL) {
-        mkdir(fullpath.c_str(), ACCESSPERMS);
-    } else {
-        closedir(dir);
+    if(!CheckExistsOrCreateDir(fullpath)) {
+        return;
     }
 }
